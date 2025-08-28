968513c2cf3d23acd5e126332041435f24409bdf
Binodini Ganeshkumar <bganeshkumar@unl.edu>
2025-08-28 17:34:45 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+ int main (int argc, char **argv) {
+
+    printf("Hello, I'm Binodini Ganeshkumar!\n");
+    printf("Major is Computer Science!\n");
+
+    return 0;
+ }
